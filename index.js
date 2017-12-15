9e032f1dd4cb8e8549ccfc453538d0e5dac6298d
sky <hyunwoo126@gmail.com>
2017-12-15 12:25:19 -0500

    test setup

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
