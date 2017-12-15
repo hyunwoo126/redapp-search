fc282badeebe85aad118031dae0b089f52cf73ce
sky <hyunwoo126@gmail.com>
2017-12-15 12:58:34 -0500

    set express port

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 5000;
 
 const express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function(request, response) {
     response.send('hey');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
   });
