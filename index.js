b0334e0fe57cc9d0fb35e081ec30e9a12e1c2166
sky <hyunwoo126@gmail.com>
2017-12-21 17:22:43 -0500

    heroku deployment bug fix

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 //myModules
-const appStores = require('./myModules/appstores');
-
-
-
-
-
+//const appStores = require('./myModules/appstores');
 
 
 var app = express();
